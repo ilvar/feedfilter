48505327b55ae1c86f05d9709db0ba956e470189
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2014-04-25 13:40:26 -0500

    weights and server-side tasks fix

--- ff.js
+++ ff.js
@@ -18,15 +18,51 @@ if (Meteor.isClient) {
 
     Template.posts.feed_url = 'http://';
 
+    Template.posts.helpers({
+        feed_title: function() {
+            return this.title || this.url;
+        },
+        current_feed: function () {
+            return Session.get("current_feed");
+        }
+    });
+
     Template.posts.events({
         'click .post-good': function (e) {
-            Posts.update(this._id, {$set: {read: true, category: 'good'}});
+            Posts.update(this._id, {$set: {read: true, category: '1 good'}});
         },
         'click .post-bad': function (e) {
-            Posts.update(this._id, {$set: {read: true, category: 'bad'}});
+            Posts.update(this._id, {$set: {read: true, category: '3 bad'}});
         },
         'change form.add_feed input': function (e) {
             Session.set('feed_url', e.target.value);
+        },
+        'change .edit-feed-window input.url': function (e) {
+            var f = Session.get("current_feed");
+            Feeds.update(f._id, {$set: {url: e.target.value}});
+        },
+        'change .edit-feed-window input.title': function (e) {
+            var f = Session.get("current_feed");
+            Feeds.update(f._id, {$set: {title: e.target.value}});
+        },
+        'change .edit-feed-window textarea.good-words': function (e) {
+            var f = Session.get("current_feed");
+            Feeds.update(f._id, {$set: {good_words: e.target.value}});
+        },
+        'change .edit-feed-window textarea.bad-words': function (e) {
+            var f = Session.get("current_feed");
+            Feeds.update(f._id, {$set: {bad_words: e.target.value}});
+        },
+        'click a.edit-feed': function() {
+            Session.set("current_feed", this);
+        },
+        'click .edit-feed-window .ff-delete-feed': function() {
+            var f = Session.get("current_feed");
+            Meteor.call('remove_feed', f._id, function() {});
+        },
+        'click .edit-feed-window .ff-save-feed': function() {
+            var f = Session.get("current_feed");
+            Feeds.update(f._id, {$set: {url: f.url, good_words: f.good_words, bad_words: f.bad_words}});
         },
         'click form.add_feed button': function (e) {
             var url = Session.get('feed_url');
@@ -36,20 +72,15 @@ if (Meteor.isClient) {
                     url: url,
                     userId: Meteor.userId()
                 });
-                console.log('New feed', fid, Meteor.userId());
             }
         }
     });
 }
 
 if (Meteor.isServer) {
+    var MyCron = new Cron(60000);
+
     Meteor.methods({
-        push_post: function(post) {
-            var existing = Posts.findOne({url: post.url, feedId: post.feedId, userId: post.userId});
-            if (!existing) {
-                Posts.insert(post);
-            }
-        },
         reset_classifiers: function () {
             Posts.remove('*.category');
             Feeds.remove('*.classifier');
@@ -58,8 +89,9 @@ if (Meteor.isServer) {
             Posts.remove({});
             Feeds.remove({});
         },
-        save_classifier: function (fid, json) {
-            Feeds.update(fid, {$set: {classifier: json}});
+        remove_feed: function(fid) {
+            Posts.remove({feedId: fid});
+            Feeds.remove(fid);
         },
         clean_feed: function(fid) {
             Posts.remove({feedId: fid});
@@ -67,5 +99,91 @@ if (Meteor.isServer) {
         }
     });
     Meteor.startup(function () {
+        function getParams(str) {
+            var params = str.split(';').reduce(function (params, param) {
+                var parts = param.split('=').map(function (part) {
+                    return part.trim();
+                });
+                if (parts.length === 2) {
+                    params[parts[0]] = parts[1];
+                }
+                return params;
+            }, {});
+            return params;
+        }
+
+        function updateFeeds() {
+            Feeds.find().forEach(function(feed) {
+                var natural = Nlp;
+
+                var restoredClassifier = feed.classifier && natural.BayesClassifier.restore(JSON.parse(feed.classifier));
+                var allow_classifying = restoredClassifier && restoredClassifier.classifier.totalExamples > 10;
+
+                var request = Npm.require("request");
+                var good_words = [];
+                var bad_words = [];
+
+                if (feed.good_words) {
+                    good_words = _.compact(_.map(feed.good_words.split('\n'), function(w) { return w.replace(' ', ''); }));
+                }
+                if (feed.bad_words) {
+                    bad_words = _.compact(_.map(feed.bad_words.split('\n'), function(w) { return w.replace(' ', ''); }));
+                }
+
+                HTTP.get(feed.url, function(err, result) {
+                    var charset = getParams(result.headers['content-type']).charset;
+                    // TODO: decoding
+
+                    xml2js.parseString(result.content, function(err, data) {
+                        console.log(data.rss.channel[0].item[0].title[0]);
+                        _.each(data.rss.channel[0].item, function(post) {
+                            var post_url = post.link[0] || post.guid[0]['_'];
+                            var full_text = post.title + ' ' + post.summary;
+                            var cat = allow_classifying && restoredClassifier.classify(full_text);
+
+                            if (_.any(bad_words, function(w) { return full_text.indexOf(w) > -1; })) {
+                                cat = '3 bad';
+                            }
+                            if (_.any(good_words, function(w) { return full_text.indexOf(w) > -1; })) {
+                                cat = '1 good';
+                            }
+
+                            var title = post.title[0];
+                            var description = post.description[0];
+
+                            var update_data = {
+                                url: post_url,
+                                feedId: feed._id,
+                                userId: feed.userId,
+                                title: title,
+                                summary: description,
+                                category: cat || '2 neutral',
+                                published_at: post.pubDate[0]
+                            }
+
+                            var existing = Posts.findOne({
+                                url: update_data.url,
+                                feedId: update_data.feedId,
+                                userId: update_data.userId
+                            });
+                            if (!existing) {
+                                Posts.insert(update_data);
+                            }
+                        });
+                    })
+                });
+
+                var classifier = new natural.BayesClassifier();
+                Posts.find({feedId: feed._id}).forEach(function(p) {
+                    classifier.addDocument(p.title + ' ' + p.summary, p.category || '2 neutral');
+                })
+                classifier.train()
+                Feeds.update(feed._id, {$set: {classifier: JSON.stringify(classifier)}});
+            });
+
+            MyCron.addJob(10, updateFeeds);
+        }
+
+        updateFeeds();
     });
 }
